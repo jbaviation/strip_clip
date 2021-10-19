2b8a082f9a08a6ac45d114f203eeb372756a1f30
Jon Borman <jborman1225@gmail.com>
2021-10-19 11:07:57 -0400

    strip_clip Script Complete
    The code works as desired and is being pushed to the remote repo in order to test the code on an alternate machine.
    
    I accidently included the virtual environment files in this repository. It bloated the repo and was removed in this commit.

--- readme.md
+++ readme.md
@@ -1,2 +1,18 @@
 # Strip Clip
-When running the script ```strip_clip.py```, the video file will be processed into clips broken down into approximately 1-minute clips.  In order to run this script, 
+This project contains a Python script designed to process a video clip into multiple 1-minute clips. When running the script ```strip_clip.py```, the video file will be processed into clips broken down into 1-minute clips.  In order to run this script, follow the installation steps below. 
+
+
+## Installation & Execution
+Required dependencies: <br>
+ - [Python (>=3.6)](https://www.python.org/)
+ - [Numpy](https://pypi.org/project/numpy/)
+ - [OpenCV](https://pypi.org/project/opencv-python/)
+
+Clone this repository, navigate into the directory, either paste the video file into the directory or enter the video filepath as an argument in the python script execution:<br> 
+```bash
+git clone https://github.com/jbaviation/strip_clip.git
+cd strip_clip
+python strip_clip.py <path.to.file>  # <path.to.file> is optional
+```
+
+And that's it!  The script will run and populate the sub-directory *VideoClips* with minute long clips.
--- strip_clip.py
+++ strip_clip.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import sys
 import os
 
 
@@ -7,21 +8,25 @@ def read_clip(path):
     # Create clip object
     clip = cv.VideoCapture(path)
 
+    # Check if output folder exists
+    if not os.path.isdir('VideoClips'):
+        os.mkdir('VideoClips')
+
     if clip.isOpened():
         frame_count = 0  # initialize counter
 
         # Get clip details
-        fps, frame_size, frame_list = get_minute_frames(clip)
+        fps, frame_size, frame_list, tot_frames = get_minute_frames(clip)
 
         # Initialize new_clip and start loop
         new_clip = None
-        while True & (frame_count<=300):
+        while True:
             # Look for new file generation
             if frame_count in frame_list:
                 _ = new_clip.release() if new_clip is not None else 0
-                filename = f'VideoClips/{frame_count}Frame.mov'
+                filename = f'VideoClips/{frame_count}Frame.mp4'
                 new_clip = cv.VideoWriter(filename,
-                                          cv.VideoWriter_fourcc('M','J','P','G'),
+                                          cv.VideoWriter_fourcc(*'mp4v'),
                                           int(fps),
                                           frame_size)
 
@@ -30,11 +35,14 @@ def read_clip(path):
 
             # If the next frame exists, track it
             if pass_:
-                # Save frame to new clip
-                print(f'Storing frame {frame_count}')
+                # Track progress and save frame to new clip
+                sys.stdout.write('\rCompleted: {:.2%}'.format(frame_count/tot_frames))
+                sys.stdout.flush()
                 new_clip.write(frame)
 
-            frame_count += 1
+                frame_count += 1
+            else:
+                break
 
         clip.release()
         new_clip.release()
@@ -53,13 +61,7 @@ def get_minute_frames(clip_obj):
     # Find clip length
     length = number_frames / fps
     minutes = int(length/60)
-    seconds = length % 60
-
-    print(f'fps = {fps}')
-    print(f'frames = {number_frames}')
-    print(f'length = {length}')
-    print(f'minutes= {minutes}')
-    print(f'seconds= {seconds}')
+    # seconds = length % 60
 
     # Get frame size
     frame_width = clip_obj.get(3)
@@ -71,9 +73,26 @@ def get_minute_frames(clip_obj):
     for minute in np.arange(1, minutes+1):
         minute_frames.append(int(fpm*minute))
 
-    return fps, frame_size, minute_frames
+    return fps, frame_size, minute_frames, number_frames
 
 
 if __name__ == '__main__':
-    read_clip(r'C:\Users\jborman\Documents\Data\3148_56041_1630792151_FishEye.mov')
+    if len(sys.argv) > 1:  # if argument is passed, use it
+        filepath = os.path.normpath(sys.argv[1])
+    else:   # otherwise look for file in the current directory
+        filepath = os.path.normpath('3148_56041_1630792151_FishEye.mov')
 
+    # Make sure file exists
+    if not os.path.isfile(filepath):
+        raise FileExistsError(f'{filepath} DOES NOT EXIST')
+
+    import time
+    start = time.time()
+    read_clip(filepath)  # read and parse the new files
+    print(f'\nTime Elapsed: {(time.time()-start)/60} Minutes')
+
+    # from line_profiler import LineProfiler
+    # lp = LineProfiler()
+    # lp_wrapper = lp(read_clip)
+    # lp_wrapper(filepath)
+    # lp.print_stats()
